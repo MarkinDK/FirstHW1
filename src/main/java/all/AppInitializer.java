195eea9c151d8aa0cdce7074bd1e70630a71dd08
Anton_Markin <anton.markin1008@yandex.ru>
2025-02-12 18:21:54 +0300

    jakarta->javax

--- src/main/java/all/AppInitializer.java
+++ src/main/java/all/AppInitializer.java
@@ -1,14 +1,17 @@
 package all;
 
 import all.configs.MyAppConfig;
-import jakarta.servlet.ServletContext;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRegistration;
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+
 public class AppInitializer implements WebApplicationInitializer {
+
     @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
         AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
